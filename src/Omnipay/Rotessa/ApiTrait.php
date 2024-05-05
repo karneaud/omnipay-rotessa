8b266a582f3a0cd12804f132660c6e6545e00665
karneaud <kendall.arneaud@gmail.com>
2024-05-05 19:17:38 -0400

    return request not response

--- src/Omnipay/Rotessa/ApiTrait.php
+++ src/Omnipay/Rotessa/ApiTrait.php
@@ -1,41 +1,41 @@
 <?php
 namespace Omnipay\Rotessa;
 
-use Omnipay\Rotessa\Message\Response\ResponseInterface;
+use Omnipay\Rotessa\Message\Request\RequestInterface;
 
 trait ApiTrait
 {
-      public function getCustomers() : ResponseInterface {
-    return $this->createRequest('GetCustomers', null )->send();  
+      public function getCustomers() : RequestInterface {
+    return $this->createRequest('GetCustomers', null );  
   }
-    public function postCustomers(array $params) : ResponseInterface {
-    return $this->createRequest('PostCustomers', $params )->send();  
+    public function postCustomers(array $params) : RequestInterface {
+    return $this->createRequest('PostCustomers', $params );  
   }
-        public function getCustomersId(array $params) : ResponseInterface {
-    return $this->createRequest('GetCustomersId', $params )->send();  
+        public function getCustomersId(array $params) : RequestInterface {
+    return $this->createRequest('GetCustomersId', $params );  
   }
-    public function patchCustomersId(array $params) : ResponseInterface {
-    return $this->createRequest('PatchCustomersId', $params )->send();  
+    public function patchCustomersId(array $params) : RequestInterface {
+    return $this->createRequest('PatchCustomersId', $params );  
   }
-        public function postCustomersShowWithCustomIdentifier(array $params) : ResponseInterface {
-    return $this->createRequest('PostCustomersShowWithCustomIdentifier', $params )->send();  
+        public function postCustomersShowWithCustomIdentifier(array $params) : RequestInterface {
+    return $this->createRequest('PostCustomersShowWithCustomIdentifier', $params );  
   }
-        public function getTransactionSchedulesId(array $params) : ResponseInterface {
-    return $this->createRequest('GetTransactionSchedulesId', $params )->send();  
+        public function getTransactionSchedulesId(array $params) : RequestInterface {
+    return $this->createRequest('GetTransactionSchedulesId', $params );  
   }
-    public function deleteTransactionSchedulesId(array $params) : ResponseInterface {
-    return $this->createRequest('DeleteTransactionSchedulesId', $params )->send();  
+    public function deleteTransactionSchedulesId(array $params) : RequestInterface {
+    return $this->createRequest('DeleteTransactionSchedulesId', $params );  
   }
-    public function patchTransactionSchedulesId(array $params) : ResponseInterface {
-    return $this->createRequest('PatchTransactionSchedulesId', $params )->send();  
+    public function patchTransactionSchedulesId(array $params) : RequestInterface {
+    return $this->createRequest('PatchTransactionSchedulesId', $params );  
   }
-        public function postTransactionSchedules(array $params) : ResponseInterface {
-    return $this->createRequest('PostTransactionSchedules', $params )->send();  
+        public function postTransactionSchedules(array $params) : RequestInterface {
+    return $this->createRequest('PostTransactionSchedules', $params );  
   }
-        public function postTransactionSchedulesCreateWithCustomIdentifier(array $params) : ResponseInterface {
-    return $this->createRequest('PostTransactionSchedulesCreateWithCustomIdentifier', $params )->send();  
+        public function postTransactionSchedulesCreateWithCustomIdentifier(array $params) : RequestInterface {
+    return $this->createRequest('PostTransactionSchedulesCreateWithCustomIdentifier', $params );  
   }
-        public function postTransactionSchedulesUpdateViaPost(array $params) : ResponseInterface {
-    return $this->createRequest('PostTransactionSchedulesUpdateViaPost', $params )->send();  
+        public function postTransactionSchedulesUpdateViaPost(array $params) : RequestInterface {
+    return $this->createRequest('PostTransactionSchedulesUpdateViaPost', $params );  
   }
      }
